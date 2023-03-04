c358b88c102857d0b6be207998f56e8a2d45761d
Jakub Gogolewski <jak.gogolewski@gmail.com>
2023-03-04 14:46:27 +0100

    dodano z zajęć

---
+++ 3_db/1_db.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+<style type>
+        
+        body{
+            background-color: lightseagreen;
+            color: snow;
+            font-family: 'Roboto', sans-serif;
+            font-size: 35px;
+            text-shadow: 2px 2px 4px black;
+        }
+        </style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h4>Użytkownicy z bazy</h4>
+<?php
+
+        require_once "../scripts/connect.php";
+
+?>
+    
+</body>
+</html>
---
+++ scripts/connect.php
@@ -0,0 +1,5 @@
+<?php
+
+$conn = new mysqli("127.0.0.1", "root", "", "wsb_baza1");
+echo "db";
+?>
